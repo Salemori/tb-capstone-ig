71fac86e0356246876d04ded350f8286481569a0
Muayyad822 <muayyad822@gmail.com>
2024-12-20 10:03:30 +0100

    Created a seperate css file in the styles folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import HomePage from './components/HomePage';
+import Post from './components/Post';
+import './styles/PostFeed.css'
+import ProfilePicture from './assets/ProfilePicture.png'
+import postImage from './assets/postImage.png'
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage/>
-    </div>
-  );
+
+const App=()=> {
+
+  return <Post username="mediamodifier" profileImage={ProfilePicture} postImage={postImage} likes='905,235' caption='#mediamodifier #mockups #design #blackfriday #blackfridaysale #sale #cybermonday' comment='103'/>
 }
 
 export default App;
